d32a12d837e3f7c196618a0c7038c0c519689efe
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-18 23:01:01 +0300

    Set up url routing

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # Include the chat app URLs
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # ALX checker wants this
 ]
+
