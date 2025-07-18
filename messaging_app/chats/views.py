042e64cbe9ca200cd009f448f20e54ff95e9a4df
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-18 22:55:19 +0300

    Built api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import (
+    ConversationSerializer,
+    ConversationCreateSerializer,
+    MessageSerializer
+)
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Override to ensure sender is always from request or validated cleanly.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
