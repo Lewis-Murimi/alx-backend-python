25d4a2b1a48d2a0cb13da18074b04616b287ae93
Lewis-Murimi <lewismurimi195@gmail.com>
2025-08-02 14:25:47 +0300

    fixed models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f'From {self.sender} to {self.receiver} at {self.timestamp}'
@@ -15,7 +16,6 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Notification for {self.user} about message {self.message.id}'
@@ -24,6 +24,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='edited_messages', null=True)
 
     def __str__(self):
         return f'Edit history for message {self.message.id} at {self.edited_at}'
